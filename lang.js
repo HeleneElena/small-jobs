889ae34cf2e3ea7d95aaa3797c122e417408b021
HeleneElena <heleneebert1@gmail.com>
2021-12-09 16:31:47 +0100

    changeLanguege

---
+++ lang.js
@@ -0,0 +1,7 @@
+const langArr = {
+   "text": { 
+    'ru': "Нас повсюду окружает природа. Иногда мы настолько привыкаем к этому, что перестаем этого замечать. Это очень плохо! Нет необходимости спешить домой после работы или учебы, рекомендую осматриваться и как можно чаще. Наслаждайтесь моментом, погодой, природой. Радуйтесь всему живому.",
+    'de': " Wir sind überall von Natur umgeben. Manchmal gewöhnen wir uns so daran, dass wir es nicht mehr bemerken. Das ist sehr schlecht! Es besteht kein Grund nach der Arbeit oder Schule nach Hause zu eilen, ich empfehle, dass Sie sich so oft wie möglich umschauen und Genieße den Moment, Wetter, Natur. Freue dich über alles Lebendige.",
+    'en': "We are surrounded by nature everywhere. Sometimes we get so used to it that we stop noticing it. This is very bad! There is no need to rush home after work or school, I recommend that you look around and as often as possible. Enjoy the moment, weather, nature. Rejoice at all living things."
+   }
+};
--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const sliderNext = document.querySelector('.slider-next'),
       sliderLine = document.querySelector('.slider-line'),
-      sliderPrev = document.querySelector('.slider-prev');
+      sliderPrev = document.querySelector('.slider-prev'),
+      select = document.querySelector('.change-lang'),
+      allLang = ['en', 'de', 'ru'];
 
 let offset = 0; // смешение от левого края
 
@@ -21,4 +23,24 @@ sliderPrev.addEventListener('click', () => {
     sliderLine.style.left = -offset + 'px'; // работатет есть в CSS position: relative;
 });
 
+select.addEventListener('change', changeURLLanguage);
 
+// перенаправлание на url с указанием языка
+function changeURLLanguage() {
+    let lang = select.value;
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+}
+
+function changeLanguege() {
+    let hash = window.location.hash;
+    hash = hash.substr(1);
+    console.log(hash);
+    if (!allLang.includes(hash)) {
+        location.href = window.location.pathname + '#en';
+        location.reload();
+    }
+    select.value = hash;
+    document.querySelector('.lng-text').innerHTML = langArr['text'][hash];
+}
+changeLanguege();
